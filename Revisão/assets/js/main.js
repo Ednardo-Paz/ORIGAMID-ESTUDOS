3eeb638247d212fd7edc52210ce71f04d5cba5af
EDNARDO DE SOUSA PAZ <eds_paz@hotmail.com>
2021-05-30 22:07:31 -0300

    Revisão
    Up

--- Revisão/assets/js/main.js
+++ Revisão/assets/js/main.js
@@ -62,38 +62,62 @@
 // })
 
 // console.log(promessa1)
-function rand(min, max) {
-  min *= 1000;
-  max *= 1000;
-  return Math.floor(Math.random() * (max - min) + min);
+// function rand(min, max) {
+//   min *= 1000;
+//   max *= 1000;
+//   return Math.floor(Math.random() * (max - min) + min);
+// }
+
+// function esperaAi(msg, tempo) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       if (typeof msg !== `string`) {
+//         reject(`Algo deu errado.`);
+//         return;
+//       }
+//       resolve(msg.toUpperCase() + ` Passeei na promise.`);
+//     }, tempo);
+//   });
+// }
+
+//   async function executar (){
+//    try{
+//     const fase1 =  await  esperaAi(`frase 1`, rand(0, 3))
+//     console.log(fase1)
+
+//     const fase2 =  await  esperaAi(2, rand(0, 3))
+//     console.log(fase2)
+
+//     const fase3 =  await  esperaAi(`frase 3`, rand(0, 3))
+//     console.log(fase3)
+
+//    }catch{
+//        console.log({ msg: `Algo deu errado.`})
+
+//    }
+//   }
+//   executar()
+
+const formCep = document.querySelector(`.formCep`);
+const inputCep = document.querySelector(`.cep`);
+const BtnCep = document.querySelector(`.btn-cep`);
+const resultado = document.querySelector(`.resultado`);
+
+BtnCep.addEventListener(`click`, handleClick);
+
+function handleClick(e) {
+  e.preventDefault();
+  const cep = inputCep.value;
+  buscaCep(cep)
 }
 
-function esperaAi(msg, tempo) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (typeof msg !== `string`) {
-        reject(`Algo deu errado.`);
-        return;
-      }
-      resolve(msg.toUpperCase() + ` Passeei na promise.`);
-    }, tempo);
-  });
+function buscaCep(cep) {
+  fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then((r) => r.text())
+    .then((r) => {
+        resultado.innerHTML = r.replace(`{`,``);
+        console.log(r)
+    });
 }
 
-  async function executar (){
-   try{
-    const fase1 =  await  esperaAi(`frase 1`, rand(0, 3))
-    console.log(fase1)
 
-    const fase2 =  await  esperaAi(2, rand(0, 3))
-    console.log(fase2)
-    
-    const fase3 =  await  esperaAi(`frase 3`, rand(0, 3))
-    console.log(fase3)
-
-   }catch{
-       console.log({ msg: `Algo deu errado.`})
-
-   }
-  }
-  executar()
