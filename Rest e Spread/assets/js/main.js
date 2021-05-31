274f7596c67e94cac4da658326684f7374d35b81
EDNARDO DE SOUSA PAZ <eds_paz@hotmail.com>
2021-05-31 14:09:20 -0300

    Rest e Spread
    Rest e Spread.

---
+++ Rest e Spread/assets/js/main.js
@@ -0,0 +1,28 @@
+// function anunciarGanhadores(premio,...ganhadores){
+//     ganhadores.forEach(ganhador => {
+//         console.log ( ganhador + ` ganhou um ` + premio )
+//     })
+// }
+
+// anunciarGanhadores(`carro`,`Eduardo`, `Monica`, `João`)
+
+// Reescreva a função utilizando
+// valores iniciais de parâmetros com ES6
+function createButton(background = `blue`, color = `red` ) {
+   
+    const buttonElement = document.createElement('button');
+    buttonElement.style.background = background;
+    buttonElement.style.color = color;
+    
+    return buttonElement;
+    }
+    const redButton = createButton();
+    console.log(redButton)
+    // Utilize o método push para inserir as frutas ao final de
+    // comidas.
+    const frutas = ['Banana', 'Uva', 'Morango'];
+    const comidas = ['Pizza', 'Batata'];
+
+    comidas.push(...frutas)
+    console.log(comidas)
+    
