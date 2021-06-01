3edb8af848c58641be618fc965df994c56cf3fd3
EDNARDO DE SOUSA PAZ <eds_paz@hotmail.com>
2021-06-01 08:56:54 -0300

    Up
    Up

--- regexp/assets/js/main.js
+++ regexp/assets/js/main.js
@@ -10,8 +10,40 @@
 // console.log(cep, cep.match(regexpCEP));
 // }
 
-const regexp = /\w+/gi;
+// const regexp = /\w+/gi;
 
-const frase = `JavaScript Linguagem 101`;
+// const frase = `JavaScript Linguagem 101`;
 
-console.log(frase.replace(regexp, `x`))
+// console.log(frase.replace(regexp, `x`))
+
+const cpfs = document.querySelectorAll(`.cpf li`);
+
+const elementsInnerText = ([...elements]) => {
+
+ return elements.map((element) => element.innerText)  
+}
+
+const limparCpf = (cpf) => {
+    return cpf.replace(/\D/g, ``);
+}
+
+const construirCpf = (cpf) =>{
+    return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, `$1.$2.$3-$4`)
+}
+
+const formatarCpfs = (cpfs) => {
+    return cpfs.map(limparCpf).map(construirCpf)
+}
+
+const substituirCPFS = (cpfsElements) => {
+    const cpfs = elementsInnerText(cpfsElements)
+    const cpfsFormatados = formatarCpfs(cpfs);
+
+    cpfsElements.forEach((element, index) => {
+        element.innerText = cpfsFormatados[index]
+    })
+    console.log(cpfsFormatados)
+}
+substituirCPFS(cpfs)
+
+ 
